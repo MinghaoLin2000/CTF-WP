2f439a1af8dd1ff4b5be404b58162ef369b6da5b
YenKoc <52554268+YenKoc@users.noreply.github.com>
2020-11-08 10:19:48 +0800

    1

--- 2020太湖杯/babyapp/app/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_ABI := armeabi-v7a
-APP_PROJECT_PATH=G:\babyapp
-APP_BUILD_SCRIPT=.\app\jni\Android.mk
--- 2020太湖杯/babyapp/app/src/main/java/com/example/babyapp/Encrypto.java
+++
@@ -1,5 +0,0 @@
-package com.example.babyapp;
-
-public class Encrypto {
-    public static native int encrypto2(String s1,String s2);
-}
